9895df04e8fb705cd5cbbfc74d21aa8d82b972a4
Sergey An <ansergeyg@gmail.com>
2021-07-15 14:02:55 +0200

    Old files removal

--- scripts/git-hooks.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo Copying git pre commit hook...
-cp scripts/git-hooks/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
--- scripts/symlink.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p bin
-ln -sfn ../vendor/consolidation/robo/robo ./bin/robo
-ln -sfn ../vendor/drush/drush/drush ./bin/drush
-ln -sfn ../vendor/behat/behat/bin/behat ./bin/behat
