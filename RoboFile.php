560ee15549b8a995be487e3de43fd695293a6a49
Sergey An <ansergeyg@gmail.com>
2021-08-26 12:48:54 +0000

    Robofile init.

---
+++ RoboFile.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+    // define public methods as commands
+  function hello($word) {
+    $this->say("Hello, $word");
+  }
+  function ls(array $args){
+    $this->taskExec('ls')->args($args)->run();
+  }
+}
