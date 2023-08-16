29cc4b98f74478e02c026eb10c9a2b143bd32673
Sergey An <ansergeyg@gmail.com>
2023-08-16 15:18:09 +0200

    Refactoring Robofile.

--- RoboFile.php
+++ RoboFile.php
@@ -4,15 +4,12 @@ use Robo\Symfony\ConsoleIO;
 
 class RoboFile extends \Robo\Tasks
 {
-
   /*
     Help command to list all available commands.
-
   */
   function drhelp($word) {
     $this->say("To install clean drupal website run: vendor/bin/robo drup");
   }
-
 
   /*
     Command to install clean drupal website.
@@ -22,9 +19,17 @@ class RoboFile extends \Robo\Tasks
     
     We have to remove them by using array_splice method. 
   */
-  function drup(ConsoleIO $io, $opts = ['account-name' => '','account-pass' => '', 'site-name' => '', 'locale' => '', 'site-mail' => '', 'db-url' => '']) {
-    $acutal_options_size = 6;
+  function drup(ConsoleIO $io, $opts = [
+    'account-name' => '',
+    'account-pass' => '', 
+    'site-name' => '', 
+    'locale' => '', 
+    'site-mail' => '', 
+    'db-url' => '']) {
+    
+    $ACTUAL_OPTIONS_SIZE = 6;
 
-    $this->taskExec('drush si standard -y')->options(array_slice($opts,0, $acutal_options_size), '=')->run();
+    $this->taskExec('drush si standard -y')->options(
+      array_slice($opts,0, $ACTUAL_OPTIONS_SIZE), '=')->run();
   }
 }
